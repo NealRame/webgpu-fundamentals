57dcb6736421acfaa9ca845342f3efe6949f9e90
Julien Graziano <julien.graziano@arobas-music.com>
2024-07-12 23:57:42 +0200

    refactor: add geometry module

--- src/lessons/9/index.ts
+++ src/lessons/9/index.ts
@@ -4,6 +4,10 @@ import {
 } from "../../colors"
 
 import {
+    createCircleVertices,
+} from "../../geometry"
+
+import {
     rand,
     times,
 } from "../../utils"
@@ -31,70 +35,6 @@ type TObjectInfo = {
     scale: number
 }
 
-type TCircleConfig = {
-    radius: number
-    innerRadius: number
-    numSubdivision: number
-    startAngle: number
-    endAngle: number
-}
-
-const CircleConfigDefault: TCircleConfig = {
-    radius: 1,
-    innerRadius: 0,
-    startAngle: 0,
-    endAngle: 2*Math.PI,
-    numSubdivision: 24,
-}
-
-type TShapeData = {
-    vertexData: Float32Array
-    vertexCount: number
-}
-
-function createCircleVertices(
-    options: Partial<TCircleConfig> = {},
-): TShapeData {
-    const {
-        endAngle,
-        innerRadius,
-        numSubdivision,
-        radius,
-        startAngle,
-    } =  { ...CircleConfigDefault, ...options }
-
-    const angle = (endAngle - startAngle)/numSubdivision
-    const vertexCount = 2*3*numSubdivision
-    const vertexData = new Float32Array(2*vertexCount)
-
-    for (let i = 0, offset = 0; i < numSubdivision; ++i) {
-        const angle1 = startAngle + (i + 0)*angle
-        const angle2 = startAngle + (i + 1)*angle
-
-        const c1 = Math.cos(angle1)
-        const s1 = Math.sin(angle1)
-        const c2 = Math.cos(angle2)
-        const s2 = Math.sin(angle2)
-
-        vertexData[offset++] = c1*radius
-        vertexData[offset++] = s1*radius
-        vertexData[offset++] = c2*radius
-        vertexData[offset++] = s2*radius
-        vertexData[offset++] = c1*innerRadius
-        vertexData[offset++] = s1*innerRadius
-        vertexData[offset++] = c1*innerRadius
-        vertexData[offset++] = s1*innerRadius
-        vertexData[offset++] = c2*radius
-        vertexData[offset++] = s2*radius
-        vertexData[offset++] = c2*innerRadius
-        vertexData[offset++] = s2*innerRadius
-    }
-
-    return {
-        vertexCount,
-        vertexData,
-    }
-}
 
 export default class extends RenderApp {
     static title_ = Title
