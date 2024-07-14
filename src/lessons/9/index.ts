7f05a7a89d9e4282d7660ae67bf056d9b961af74
Julien Graziano <julien.graziano@arobas-music.com>
2024-07-14 13:55:33 +0200

    refactor: delete geometry module

--- src/lessons/9/index.ts
+++ src/lessons/9/index.ts
@@ -4,10 +4,6 @@ import {
 } from "../../colors"
 
 import {
-    createCircleVertices,
-} from "../../geometry"
-
-import {
     rand,
     times,
 } from "../../utils"
@@ -17,6 +13,73 @@ import {
 } from "../render"
 
 import shaderSource from "./shader.wgsl?raw"
+
+
+type TCircleConfig = {
+    radius: number
+    innerRadius: number
+    numSubdivision: number
+    startAngle: number
+    endAngle: number
+}
+
+const CircleConfigDefault: TCircleConfig = {
+    radius: 1,
+    innerRadius: 0,
+    startAngle: 0,
+    endAngle: 2*Math.PI,
+    numSubdivision: 24,
+}
+
+type TCircleData = {
+    vertexData: Float32Array
+    vertexCount: number
+}
+
+function createCircleVertices(
+    options: Partial<TCircleConfig> = {},
+): TCircleData {
+    const {
+        endAngle,
+        innerRadius,
+        numSubdivision,
+        radius,
+        startAngle,
+    } =  { ...CircleConfigDefault, ...options }
+
+    const angle = (endAngle - startAngle)/numSubdivision
+    const vertexCount = 2*3*numSubdivision
+    const vertexData = new Float32Array(2*vertexCount)
+
+    for (let i = 0, offset = 0; i < numSubdivision; ++i) {
+        const angle1 = startAngle + (i + 0)*angle
+        const angle2 = startAngle + (i + 1)*angle
+
+        const c1 = Math.cos(angle1)
+        const s1 = Math.sin(angle1)
+        const c2 = Math.cos(angle2)
+        const s2 = Math.sin(angle2)
+
+        vertexData[offset++] = c1*radius
+        vertexData[offset++] = s1*radius
+        vertexData[offset++] = c2*radius
+        vertexData[offset++] = s2*radius
+        vertexData[offset++] = c1*innerRadius
+        vertexData[offset++] = s1*innerRadius
+        vertexData[offset++] = c1*innerRadius
+        vertexData[offset++] = s1*innerRadius
+        vertexData[offset++] = c2*radius
+        vertexData[offset++] = s2*radius
+        vertexData[offset++] = c2*innerRadius
+        vertexData[offset++] = s2*innerRadius
+    }
+
+    return {
+        vertexCount,
+        vertexData,
+    }
+}
+
 
 const Title = "Triangle shaders with storage buffers."
 const Description = "Draw several triangle in one pass using storage buffer."
@@ -34,7 +97,6 @@ const ObjectCount = 4
 type TObjectInfo = {
     scale: number
 }
-
 
 export default class extends RenderApp {
     static title_ = Title
