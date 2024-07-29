7a9d654858d176074d375298906ab561be0b7e07
Julien Graziano <julien.graziano@arobas-music.com>
2024-07-29 23:20:08 +0200

    refactor: update home page style

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -22,31 +22,33 @@ const linkStyle = ["text-blue", "hover:text-green"]
         <h1 class="border-b border-green mb-4 pb-2 text-center text-green text-4xl">Lessons</h1>
         <ol class="chapter-list flex flex-col gap-4">
             <li v-for="(chapter, chapterIndex) in Chapters">
-                <span class="text-lg">
-                    <RouterLink
-                        :class="linkStyle"
-                        :to="{
-                            name: 'chapter',
-                            params: {
-                                chapterIndex,
-                            },
-                        }"
-                    >{{ chapter.name }}</RouterLink>
-                </span>
-                <ol class="lesson-list px-8">
-                    <li v-for="(Lesson, lessonIndex) in chapter.lessons">
+                <div>
+                    <span class="text-lg">
                         <RouterLink
                             :class="linkStyle"
                             :to="{
-                                name: 'lesson',
+                                name: 'chapter',
                                 params: {
                                     chapterIndex,
-                                    lessonIndex,
                                 },
                             }"
-                        >{{ Lesson.title }}</RouterLink>
-                    </li>
-                </ol>
+                        >{{ chapter.name }}</RouterLink>
+                    </span>
+                    <ol class="lesson-list">
+                        <li v-for="(Lesson, lessonIndex) in chapter.lessons">
+                            <RouterLink
+                                :class="linkStyle"
+                                :to="{
+                                    name: 'lesson',
+                                    params: {
+                                        chapterIndex,
+                                        lessonIndex,
+                                    },
+                                }"
+                            >{{ Lesson.title }}</RouterLink>
+                        </li>
+                    </ol>
+                </div>
             </li>
         </ol>
     </div>
